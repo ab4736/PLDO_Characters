ccdc99f1682e4027d5bfad36a0d0e5d23efe4cbf
akabho <akash.bhowmick@hotmail,com>
2025-10-02 13:13:09 -0400

    Setup

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+"""Tests for utils.py."""
+
+from PLDO_Characters.structs import Dog
+from PLDO_Characters.utils import get_good_dogs_of_breed
+
+
+def test_get_good_dogs_of_breed():
+    """Tests for get_good_dogs_of_breed()."""
+    ralph = Dog("ralph", "corgi")
+    maeve = Dog("maeve", "corgi")
+    frank = Dog("frank", "poodle")
+    dogs = {ralph, maeve, frank}
+    assert get_good_dogs_of_breed(dogs, "corgi") == {ralph, maeve}
